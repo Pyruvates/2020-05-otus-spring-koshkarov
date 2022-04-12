b112821383c913df9cba67bc42a798011fa802b6
Vitaliy Koshkarov <redrick.shuhart123@gmail.com>
2022-04-12 12:36:38 +0300

    Added dependencies, description and beans.xml configuration

--- src/main/java/ru/koshkarovvitaliy/Main.java
+++ src/main/java/ru/koshkarovvitaliy/Main.java
@@ -1,7 +1,14 @@
 package ru.koshkarovvitaliy;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Spring!");
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        User user = context.getBean(User.class);
+        System.out.println("Name: " + user.getName() + ", age: " + user.getAge());
     }
 }
