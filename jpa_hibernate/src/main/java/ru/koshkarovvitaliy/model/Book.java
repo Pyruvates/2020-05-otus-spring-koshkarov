4d8adb8b767f43517f35a2bacee3ad1ad3c6425f
Vitaliy Koshkarov <redrick.shuhart123@gmail.com>
2023-03-11 14:25:57 +0300

    Project has been created; added model

---
+++ jpa_hibernate/src/main/java/ru/koshkarovvitaliy/model/Book.java
@@ -0,0 +1,14 @@
+package ru.koshkarovvitaliy.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    private String name;
+    private Author author;
+    private Genre genre;
+}
