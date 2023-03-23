0de25979dc8fc99e89eba73ef172a5936ceeba55
Vitaliy Koshkarov <redrick.shuhart123@gmail.com>
2023-03-23 22:27:36 +0300

    Implemented CRUD on JPQL

--- jpa_hibernate/src/main/resources/data.sql
+++ jpa_hibernate/src/main/resources/data.sql
@@ -1,13 +1,13 @@
 INSERT INTO genres(name) VALUES
-('Sci-Fi'), ('Fantasy'), ('Novel'), ('Historical fiction'), ('Biography'), ('Reference work'), ('Programming');
+('Sci-Fi'), ('Fantasy'), ('Reference work'), ('Programming');
 
 INSERT INTO authors(first_name, last_name) VALUES
-('Joanne', 'Rowling'), ('Robert', 'Martin'), ('Joshua', 'Bloch');
+('Joanne', 'Rowlink'), ('Robert', 'Martin'), ('Joshua', 'Bloch');
 
 INSERT INTO books(name, author_id, genre_id) VALUES
 ('Harry Potter and the Deathly Hallows - Part 1', 1, 2),
-('Clean Code', 2, 7),
-('Effective Java', 3, 6),
+('Clean Code', 2, 4),
+('Effective Java', 3, 3),
 ('Harry Potter and the Deathly Hallows - Part 2', 1, 2),
 ('Harry Potter and the Half-Blood Prince', 1, 2),
-('Clean Architecture: A Craftsman Guide to Software Structure and Design', 2, 7);
+('Clean Architecture: A Craftsman Guide to Software Structure and Design', 2, 4);
