512a9c3c9bb5e531859598e5ee8775124dd111b7
Vitaliy Koshkarov <redrick.shuhart123@gmail.com>
2023-07-12 22:54:38 +0300

    Refactoring parameters of genre controller methods

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ List of projects:
 3. Spring-boot students application based on previous apps. Module: `spring-boot-students-app`
 4. JPA (Hibernate) app for saving books, authors and genres. Module: `jpa_hibernate`
 5. Spring Data JPA book storage application (based on `jpa-hibernate`)
-6. Spring MVC CRUD application using Thymeleaf. Module: `spring-mvn-thymeleaf`
+6. Spring MVC CRUD application using Thymeleaf. Module: `spring-mvc-thymeleaf`
--- spring-mvc-thymeleaf/src/main/java/ru/koshkarovvitaliy/controller/GenreController.java
+++ spring-mvc-thymeleaf/src/main/java/ru/koshkarovvitaliy/controller/GenreController.java
@@ -2,8 +2,8 @@ package ru.koshkarovvitaliy.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import ru.koshkarovvitaliy.model.Genre;
@@ -19,20 +19,20 @@ public class GenreController {
     private final GenreService genreService;
 
     @GetMapping(path = "/genre")
-    public ModelAndView genres() {
+    public ModelAndView genres(@Value("genre/genre.html") ModelAndView genresModelAndView) {
         List<Genre> genres = genreService.getAllGenres();
 
         log.info("All genres {}", genres);
 
-        ModelAndView genresModelAndView = new ModelAndView("genre/genre.html");
         genresModelAndView.addObject("genres", genres);
 
         return genresModelAndView;
     }
 
     @GetMapping(path = "/genre/addNewGenre")
-    public ModelAndView addNewGenre(@ModelAttribute("genre") final Genre genre) {
-        return new ModelAndView("genre/new.html");
+    public ModelAndView addNewGenre(@Value("genre/new.html") ModelAndView addGenreModelAndView,
+                                    @ModelAttribute("genre") final Genre genre) {
+        return addGenreModelAndView;
     }
 
     @PostMapping(path = "/genre/saveNewGenre", params = "add")
@@ -50,26 +50,30 @@ public class GenreController {
     }
 
     @GetMapping(path = "genre/edit")
-    public ModelAndView editGenre(@RequestParam("id") final Integer id, Model editModel) {
+    public ModelAndView editGenre(@Value("genre/edit.html") ModelAndView editModelAndView,
+                                  @RequestParam("id") final Integer id) {
         Genre genre = genreService.findById(id);
 
-        log.info("Found genre by id {}", genre);
+        log.info("Genre found by id {}", genre);
 
-        editModel.addAttribute("genre", genre);
+        editModelAndView.addObject("genre", genre);
 
-        return new ModelAndView("genre/edit.html");
+        return editModelAndView;
     }
 
     @PostMapping(path = "genre/edit", params = "save")
     public ModelAndView saveEditedGenre(@ModelAttribute("genre") final Genre genre) {
         Genre editedGenre = genreService.saveNewGenre(genre);
+
         log.info("Saved genre after edit {}", editedGenre);
+
         return new ModelAndView("redirect:/library/genre");
     }
 
     @PostMapping(path = "genre/edit", params = "cancel")
     public ModelAndView cancelSaveEditedGenre(@ModelAttribute("genre") final Genre genre) {
         log.info("Cancel edit {}", genre);
+
         return new ModelAndView("redirect:/library/genre");
     }
 
